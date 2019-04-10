f785a5512483059f5917eccf7cdff5803705da15
Jason Dees <jdees@pureromance.com>
2019-04-10 12:44:11 -0400

    Vue form stuff

--- MapR/Features/AddGame/Models/AddGame.cs
+++ MapR/Features/AddGame/Models/AddGame.cs
@@ -2,7 +2,7 @@
 namespace MapR.Features.AddGame.Models {
     public class AddGame { 
         public string Name { get; set; }
-        public bool IsPublic { get; set; }
+        public bool IsPrivate { get; set; }
 
     }
 }
