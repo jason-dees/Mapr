9b349d72bacd224725348e40cacb9d32c1bdc59f
Jason Dees <jdees@pureromance.com>
2019-05-13 10:03:57 -0400

    updating cmd file

--- MapR/deploy.cmd
+++ MapR/deploy.cmd
@@ -67,24 +67,27 @@ SET MSBUILD_PATH=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
 echo Handling ASP.NET Core Web Application deployment.
 
 :: 1. Restore nuget packages
+echo 1. Restore nuget packages 
 call :ExecuteCmd dotnet restore "%DEPLOYMENT_SOURCE%\MapR.sln"
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 2. Build and publish
+echo 2. Build and publish
 call :ExecuteCmd dotnet publish "%DEPLOYMENT_SOURCE%\MapR\MapR.csproj" --output "%DEPLOYMENT_TEMP%" --configuration Release
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 3. Install npm dependencies for app and build
-echo 1. Installing npm packages for app and build in %~dp0% 
+echo 3. Installing npm packages for app and build in %~dp0% 
 call :ExecuteCmd npm install
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 4. Build
-echo 2. Building app 
+echo 4. Building app 
 call :ExecuteCmd npm run build
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 5. KuduSync
+echo 5. Kudu syncing built app from dist folder to deployment target
 call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
 IF !ERRORLEVEL! NEQ 0 goto error
 
