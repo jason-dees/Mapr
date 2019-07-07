01705059a0b6195379f684d2a5a5e6c4315f5f88
Jason Dees <jhdees@gmail.com>
2019-07-06 20:53:01 -0400

    Scss stuff

--- MapR.Vue/src/features/index/Index.vue
+++ MapR.Vue/src/features/index/Index.vue
@@ -43,16 +43,15 @@ export default {
 }
 </script>
 
-<style>
-body {
-  margin:0px;
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import '../shared/main.scss';
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- MapR.Vue/src/features/nav/MapRNav.vue
+++ MapR.Vue/src/features/nav/MapRNav.vue
@@ -1,15 +1,25 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <router-link to="/">Home</router-link>
-        <a v-bind:href="functionsUrl+'api/login/google?redirect=' + appServerUrl">google</a>
-        <span v-if="userInfo.loadedUserInfo && userInfo.user !== ''">
-            {{userInfo.user}}
-        </span>
-        <span v-else-if="userInfo.loadedUserInfo && userInfo.user === ''">
-            I need to do javascript app auth with google or whomever
-        </span>
-        <span v-else>loading user information</span>
-        <a v-bind:href="functionsUrl+'api/logout'">logout</a>
+    <nav class="navbar">
+        <div class="nav-wrapper">
+            <ul>
+                <li><router-link to="/">Home</router-link></li>
+                <li class="right" v-if="!userInfo.loadedUserInfo || userInfo.user === ''">
+                    <a v-bind:href="functionsUrl+'api/login/google?redirect=' + appServerUrl">google</a>
+                </li>
+                <li class="right" v-if="userInfo.loadedUserInfo && userInfo.user !== ''">
+                    <a v-bind:href="functionsUrl+'api/logout'">logout</a>
+                </li>
+                <li class="right">
+                    <span v-if="userInfo.loadedUserInfo && userInfo.user !== ''">
+                        {{userInfo.user}}
+                    </span>
+                    <span v-else-if="userInfo.loadedUserInfo && userInfo.user === ''">
+                        Login with a provider
+                    </span>
+                    <span v-else>loading user information</span>
+                </li>
+            </ul>
+        </div>
     </nav> 
 </template>
 <script>
@@ -23,5 +33,38 @@ export default{
 }
 </script>
 <style lang="scss" scoped>
-    @import 'MapRNav.scss'
+    @import '../shared/variables.scss';
+    .navbar{
+        position:fixed;
+        width:100%;
+        top:0px;
+        height:$menu-height + 'px';
+        .nav-wrapper{
+            padding-top: 0px;
+            background-color:$papyrus;
+            ul {
+                margin:0px;
+                list-style: none;
+                li {
+                    display: inline-block;
+                    * {
+                        -webkit-transition: background-color .3s;
+                        transition: background-color .3s;
+                        font-size: 1rem;
+                        color: $link-color;
+                        display: block;
+                        padding:15px;
+                        text-decoration: none; 
+                    }
+                    a:hover{
+                        background-color:$papyrus-dark;
+                        cursor: pointer;
+                    }
+                    span {
+                        color: black;
+                    }
+                }
+            }
+        }
+    }
 </style>
