fbfcb7896decded94e38f24f494bed4590d9ed66
Jason Dees <jhdees@gmail.com>
2020-02-22 07:40:56 -0500

    Working on getting anonymous signalr working

--- MapR.Functions/Functions/SignalRFunctions.cs
+++ MapR.Functions/Functions/SignalRFunctions.cs
@@ -8,16 +8,33 @@ using MapR.Functions.Extensions;
 using System.Linq;
 using Newtonsoft.Json;
 using MapR.Functions.Models.Messages;
+using System;
+using Microsoft.Extensions.Primitives;
 
 namespace MapR.Functions {
 	public static class SignalRFunctions {
 		[FunctionName("negotiate")]
 		public static SignalRConnectionInfo Negotiate(
 			[HttpTrigger(AuthorizationLevel.Anonymous)]HttpRequest req,
-			[SignalRConnectionInfo(HubName = "mapr", UserId = "{headers.x-ms-client-principal-name}")]
-			SignalRConnectionInfo connectionInfo){
-				// connectionInfo contains an access key token with a name identifier claim set to the authenticated user
-				return connectionInfo;
+			[SignalRConnectionInfo(HubName = "mapr")]
+			SignalRConnectionInfo connectionInfo,
+			ClaimsPrincipal user,
+			IBinder binder){
+			var userId = req.Headers["{headers.x-ms-client-principal-name}"];//{} for unauthenticated person
+			if (userId == default(StringValues)){
+				userId = Guid.NewGuid().ToString();
+				var identity = new ClaimsIdentity();
+				identity.AddClaim(new Claim(ClaimTypes.Name, userId));
+				user.AddIdentity(identity);
+			}
+			
+			// connectionInfo contains an access key token with a name identifier claim set to the authenticated user
+			SignalRConnectionInfoAttribute attribute = new SignalRConnectionInfoAttribute {
+				HubName = "mapr",
+				UserId = userId
+			};
+			SignalRConnectionInfo connection = binder.BindAsync<SignalRConnectionInfo>(attribute).Result;
+			return connection;
 		}
 
 		[FunctionName("AddToGame")]
